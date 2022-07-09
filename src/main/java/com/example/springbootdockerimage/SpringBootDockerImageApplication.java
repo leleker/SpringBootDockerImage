09ba4d6af6a3a352181981998743182a3571764e
leleker <eswara.psr@gmail.com>
2022-07-09 16:10:31 +0530

    test

--- src/main/java/com/example/springbootdockerimage/SpringBootDockerImageApplication.java
+++ src/main/java/com/example/springbootdockerimage/SpringBootDockerImageApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerImageApplication {
+    @GetMapping
+    public String message(){
+        return " hello docker....";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDockerImageApplication.class, args);
