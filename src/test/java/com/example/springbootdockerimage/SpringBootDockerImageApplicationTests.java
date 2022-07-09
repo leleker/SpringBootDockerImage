4e5bd90d4982dbc54847f81c5612c92948b66075
leleker <eswara.psr@gmail.com>
2022-07-09 15:23:20 +0530

    first commit

---
+++ src/test/java/com/example/springbootdockerimage/SpringBootDockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
